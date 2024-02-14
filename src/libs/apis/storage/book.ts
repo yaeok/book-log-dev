96031d61bfbf68c84d324e897e650fc6d60a7f81
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2024-02-14 10:35:28 +0900

    firebase storageを追加

---
+++ src/libs/apis/storage/book.ts
@@ -0,0 +1,51 @@
+import loadImage from 'blueimp-load-image'
+
+import { storage } from '@/libs/config'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+
+export const registerBookFromStorage = async (args: {
+  file: File[]
+  uid: string
+}): Promise<string> => {
+  const urlList = new Array<string>()
+  for (const item of args.file) {
+    /** 画像圧縮 */
+    const canvas = await loadImage(item, {
+      maxWidth: 300,
+      maxHeight: 300,
+      canvas: true,
+    })
+    if (canvas.image instanceof HTMLCanvasElement) {
+      canvas.image.toBlob(async (blob: Blob | null) => {
+        if (blob) {
+          const change = new File([blob], item.name, {
+            type: blob.type,
+            lastModified: Date.now(),
+          })
+          const fullPath = 'books' + args.uid + item.name
+          const uploadRef = ref(storage, fullPath)
+
+          await uploadBytes(uploadRef, change)
+        }
+      })
+    }
+    urlList.push(item.name)
+  }
+  return urlList[0]
+}
+
+export const getBookImageUrl = async (imageURL: string): Promise<string> => {
+  const reference = ref(
+    storage,
+    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_URL + imageURL
+  )
+  const url = getDownloadURL(reference)
+    .then((url) => {
+      return url
+    })
+    .catch((err) => {
+      console.log(err)
+      return ''
+    })
+  return url
+}
